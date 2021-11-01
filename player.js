53e070dce83a693636be02b106234bdd0dc4a154
AdelAlkhesh <splinteredsanity@outlook.com>
2021-11-01 15:19:02 +0100

    fixed typo

--- player.js
+++ player.js
@@ -2,7 +2,7 @@ class Player {
   constructor(x, y, radius, color, velocity) {
     this.x = x;
     this.y = y;
-    this.rarius = radius;
+    this.radius = radius;
     this.color = color;
     this.velocity = 3;
     this.angle = 1;
@@ -10,7 +10,7 @@ class Player {
 
   drawPlayer() {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.rarius, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
