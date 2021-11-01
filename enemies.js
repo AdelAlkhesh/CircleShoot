32c39239791c4b5bec042a55554538eb63ad9ce8
AdelAlkhesh <splinteredsanity@outlook.com>
2021-11-01 15:18:28 +0100

    enemies class

---
+++ enemies.js
@@ -0,0 +1,23 @@
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  drawEnemy() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  update() {
+    this.drawEnemy();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
