df9bc58b2d02adfc8bb5d03c738bfaf7f9cd2116
AdelAlkhesh <splinteredsanity@outlook.com>
2021-11-03 15:59:37 +0100

    added comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,81 @@
+//----------------- DOM RELATED -------------------------
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+canvas.height = innerHeight;
+canvas.width = innerWidth;
+//-------------------------------------------------------
 
+// ---------------------- AUDIO -------------------------
+let audio = new Audio("./audio/Sub - Mini Impact-[AudioTrimmer.com] (1).wav");
+let backgroundAudio = new Audio("./audio/Sweet baby kicks PSG.mp3");
+//-------------------------------------------------------------------------
+
+// ------------- Variables for collision and movement -------//
 let isLeft = false;
 let isRight = false;
 let isUp = false;
 let isDown = false;
+// -------------------------------------------------------------
 
+// ------------------ misc variables ----------------------------
 let proRadius = 5;
-
-let audio = new Audio("./audio/Sub - Mini Impact-[AudioTrimmer.com] (1).wav");
-let backgroundAudio = new Audio("./audio/Sweet baby kicks PSG.mp3");
-
 let difficulty = 2;
+let animationID;
+// --------------------------------------------------------------
 
+//----------------------- Power Up variables---------------------
 let timer = 0;
 let speedID = 0;
 let cannonID = 0;
+let powerUpDropped = false;
+//---------------------------------------------------------------
 
-letbackgroundColor = `hsl(${Math.random() * 360}, 50%, 50%)`;
 
-canvas.height = innerHeight;
-canvas.width = innerWidth;
-
+// ------------------ Player Spawn and coordinates ------------------------ 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 const player = new Player(x, y, 15, "white");
+//-------------------------------------------------------------------------
 
+
+//------------------------ Arrays for spawning game objects ---------------------
 const projectiles = [];
 const enemies = [];
 const particles = [];
 const powerUps = [];
 const randomDrops = ["cannon", "speed", "health"];
-let powerUpDropped = false;
+//---------------------------------------------------------------------------------
 
 function spawnEnemies() {
   setInterval(() => {
     let health = 0;
-    let radius = Math.floor(Math.random() * 40);
+    let radius = Math.floor(Math.random() * 40); //create a random radius between 1 and 40
     if (radius < 10) {
       radius += 10;
     }
     if (radius > 30) {
-      health = 1;
+      health = 1;    //if enemy radius is greater than 30, it has 2 points of health
     }
+
+    //enemy x and y coordinates
     let x;
     let y;
-
     if (Math.random() < 0.5) {
-      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius; 
       y = Math.random() * canvas.height;
     } else {
       y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
       x = Math.random() * canvas.width;
     }
     let color1 = Math.random() * 360;
-    const color = `hsl(${color1}, 50%, 50%`;
-    const angle = Math.atan2(player.y - y, player.x - x);
-    let velocity = {
+    const color = `hsl(${color1}, 50%, 50%`;  // randomized color 
+    const angle = Math.atan2(player.y - y, player.x - x);  //calculate angle based on player position - enemy spawn position
+    let velocity = {  // use the angle to determine the direction and velocity of the enemy (to move towards the player)
       x: Math.cos(angle) * difficulty,
       y: Math.sin(angle) * difficulty,
     };
 
-    enemies.push(new Enemy(x, y, radius, color, velocity, health));
+    enemies.push(new Enemy(x, y, radius, color, velocity, health));  // create a new enemy object based on the above variables ( it will spawn every 1 second from a random spot outside the canvas boundries and move in)
   }, 600);
 }
 
@@ -115,20 +129,28 @@ function playerMovement() {
     player.y += player.velocity;
   }
 }
-let animationID;
+
+
+//--------------------------------- MAIN GAME LOOP AND ANIMATION ----------------------------------------------------------
+
 function animate() {
   backgroundAudio.play();
   backgroundAudio.volume = 0.2;
-
+//---------------------------------
   animationID = requestAnimationFrame(animate);
+  //--------------- blur effect ------------
   ctx.fillStyle = "rgba(0,0,0,0.3";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //------------------------------------------
+  //-------------game object draw functions -----------
   player.drawPlayer();
   drawScore();
   drawLives();
   drawDifficulty();
   increaseDifficulty();
   playerMovement();
+//------------------------------------------------------
+  
   powerUps.forEach((ele) => {
     ele.update();
   });
@@ -146,21 +168,21 @@ function animate() {
   });
   enemies.forEach((enemy, index) => {
     enemy.update();
-    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
-    if (dist - enemy.radius - player.radius < 1 && player.lives == 0) {
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y); //calculate distance by substracting enemy position from player position (x, y)
+    if (dist - enemy.radius - player.radius < 1 && player.lives == 0) {  //collision check, if player has 0 health, freeze animation
       backgroundAudio.pause();
       cancelAnimationFrame(animationID);
-    } else if (dist - enemy.radius - player.radius < 1 && player.lives > 0) {
+    } else if (dist - enemy.radius - player.radius < 1 && player.lives > 0) { //if player has health > 0, destroy enemy and remove 1 health from player
       setTimeout(() => {
         player.lives -= 1;
         enemies.splice(index, 1);
       }, 0);
     }
     projectiles.forEach((projectile, proIndex) => {
-      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y); //same distance calculation as above, but this one is for projectiles and enemies
       if (dist - enemy.radius - projectile.radius < 1 && enemy.health == 0) {
-        let chance = Math.round(Math.random() * 35);
-        if (
+        let chance = Math.round(Math.random() * 35); //create a random number generator on enemy death
+        if ( // drop a powerup 
           chance == 1 &&
           player.hasPowerUp == false &&
           powerUpDropped == false
@@ -171,13 +193,13 @@ function animate() {
               projectile.x,
               projectile.y,
               15,
-              randomDrops[Math.floor(Math.random() * randomDrops.length)]
+              randomDrops[Math.floor(Math.random() * randomDrops.length)] //push random powerup into the powerup array 
             )
           );
         }
 
         for (let i = 0; i < 8; i++) {
-          particles.push(
+          particles.push(  //push particles into the particle array when a projectile collides with an enemy. this produces explosion effect
             new Particle(projectile.x, projectile.y, 3, enemy.color, {
               x: Math.random() - 0.5,
               y: Math.random() - 0.5,
@@ -208,7 +230,7 @@ function animate() {
       }
     });
   });
-  projectiles.forEach((ele, proIndex) => {
+  projectiles.forEach((ele, proIndex) => { // destroy projectiles when they leave screen boundries by looping through the projectile array and checking each projectile position 
     if (ele.x + ele.radius > canvas.width) {
       setTimeout(() => {
         projectiles.splice(proIndex, 1);
@@ -231,19 +253,19 @@ function animate() {
     }
   });
 
-  powerUps.forEach((drop, index) => {
+  powerUps.forEach((drop, index) => { //check if player collides with powerup by looping through powerup array and checking position relative to player position
     let powerUpDist = Math.hypot(player.x - drop.x, player.y - drop.y);
     if (
       powerUpDist - drop.radius - player.radius < 1 &&
       drop.name == "health"
-    ) {
+    ) { //the health drop adds one health to the player
       player.lives += 1;
       powerUps.splice(index, 1);
       powerUpDropped = false;
     } else if (
       powerUpDist - drop.radius - player.radius < 1 &&
       drop.name == "speed"
-    ) {
+    ) { //the speed drop sets the move speed to 6 for 20 seconds
       player.hasPowerUp = true;
       player.velocity = 6;
       powerUps.splice(index, 1);
@@ -257,7 +279,7 @@ function animate() {
           powerUpDropped = false;
         }
       }, 1000);
-    } else if (
+    } else if ( //the cannon drop increases projectile radius to 20 for 20 seconds
       powerUpDist - drop.radius - player.radius < 1 &&
       drop.name == "cannon"
     ) {
@@ -277,10 +299,9 @@ function animate() {
     }
   });
 }
+//-------------------------------------------------------------------------------------------------------------------------
 
-function speedUP() {}
-
-function shootProjectile() {
+function shootProjectile() { //same calculations for enemy movement to follow player. but for projectiles they move towards the mouse click coordinates (clientY, clientX )
   const angle = Math.atan2(event.clientY - player.y, event.clientX - player.x);
   let velocity = {
     x: Math.cos(angle) * 10,
@@ -328,7 +349,9 @@ addEventListener("keyup", (event) => {
     isDown = false;
   }
 });
-animate();
 
+
+
+animate();
 spawnEnemies();
 increaseDifficulty();
